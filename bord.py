4f517ebaeb3c2673d2990a3631f0c8882eb8867a
Barnaby Colby <barnaby.colby@gmail.com>
2018-03-05 19:55:20 +0000

    Download bottles instead of formluas

--- bord.py
+++ bord.py
@@ -5,7 +5,9 @@ import os
 import requests
 import sys
 
-BASE_URL = "http://formulae.brew.sh"
+BASE_FORMULA_URL = "http://formulae.brew.sh"
+BASE_BOTTLE_URL = "https://bintray.com/homebrew/bottles/download_file?file_path="
+OS = "high_sierra"
 
 def get_dependency_list(package):
     package_name = package['name']
@@ -14,24 +16,19 @@ def get_dependency_list(package):
     page = requests.get(package_url)
     tree = html.fromstring(page.content)
 
+    package_version = tree.xpath("//table[@class='versions']//strong[@class='version']/text()")[0]
+
     # Parse the dependencies and their urls
     package_deps = []
     package_deps_nodes = tree.xpath("//div[@id='deps']//a[@class='formula']")
     for node in package_deps_nodes:
         dep_name = node.xpath("text()")[0]
         dep_relative_url = node.xpath("@href")[0]
-        dep_url = "{}{}".format(BASE_URL, dep_relative_url)
+        dep_url = "{}{}".format(BASE_FORMULA_URL, dep_relative_url)
         package_dep = { 'name': dep_name, 'url': dep_url }
         package_deps.append(package_dep)
     
-    package_urls = tree.xpath("//a[text()='Formula code']/@href")
-
-    if len(package_urls) is not 1:
-        print("Couldn't find the formula URL for {}. The scraping is probably broken!".format(package))
-        sys.exit(1)
-    package_url = package_urls[0]
-
-    return { 'name': package_name, 'url': package_url, 'deps': package_deps }
+    return { 'name': package_name, 'url': package_url, 'deps': package_deps, 'version': package_version }
 
 def package_in_dictionaries(package, dictionaries):
     for dictionary in dictionaries:
@@ -71,19 +68,32 @@ def download_formulas(package_dictionaries, output_dir):
         with open(file_path, "wb") as output_file:
             output_file.write(request.content)
 
+def download_bottles(package_dictionaries, output_dir):
+    for package_dictionary in package_dictionaries:
+        name = package_dictionary['name']
+        version = package_dictionary['version']
+        package_path = "{}-{}.{}.bottle.tar.gz".format(name, version, OS)
+        url = "{}{}".format(BASE_BOTTLE_URL, package_path)
+
+        print("Downloading bottle for {}.".format(name))
+        request = requests.get(url)
+        file_path = os.path.join(output_dir, package_path)
+        with open(file_path, "wb") as output_file:
+            output_file.write(request.content)
+
 def main():
     if len(sys.argv) is not 2:
         print("Usage: {} <package name>".format(sys.argv[0]))
         sys.exit(1)
 
     package_name = sys.argv[1]
-    package_url = "{}/formula/{}".format(BASE_URL, package_name)
+    package_url = "{}/formula/{}".format(BASE_FORMULA_URL, package_name)
     package = { 'name': package_name, 'url': package_url }
     dependencies = get_full_dependency_list(package)
 
     if not os.path.exists(package_name):
         os.makedirs(package_name)
-    download_formulas(dependencies, package_name)
+    download_bottles(dependencies, package_name)
 
 if __name__ == "__main__":
     main()
